d38b212533ff717b2c8f7f8e42dd90402796e9c3
Anshu010412 <Anshujhacoc@gmail.com>
2026-02-06 15:52:37 +0530

    initialise file

--- src/Context/AuthProvider.jsx
+++ src/Context/AuthProvider.jsx
@@ -1,7 +1,6 @@
 import React, { createContext, useEffect, useState } from 'react'
 import { getLocalStorage, setLocalStorage } from '../utils/LocalStorage'
 
-
 // eslint-disable-next-line react-refresh/only-export-components
 export const AuthContext = createContext()
 // localStorage.clear()
